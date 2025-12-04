f088a14e46495fc4cf35cc2eaac025d1d0fec0f6
maxschoolgit <maxw76@nycstudents.net>
2025-12-04 11:58:16 -0500

    Update stopLight.py

--- stopLight.py
+++ stopLight.py
@@ -2,22 +2,28 @@ from tkinter import *
 #need to install on all machines
 from tkmacosx import Button
 
+
 # Create the main window
 root = Tk()
 root.title("Enter Title Here")
 
 #Set size of window
-root.geometry("300x150")
+root.geometry("500x400")
+
 
 # Create buttons
 red_button = Button(root, text="Red", background='red')
+green_button = Button(root, text='Green', background='green')
+yellow_button = Button(root, text='Yellow', background='yellow')
 
 #Add a label
-label = Label(root, text="CHANGE ME!")
+label = Label(root, text="Red Light, Green Light!")
 
 # Place widgets in window (with pack function!)
 label.pack()
 red_button.pack()
+green_button.pack()
+yellow_button.pack()
 
 
 # Start the GUI event loop
