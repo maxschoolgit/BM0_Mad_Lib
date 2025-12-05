3b148fd84cf65027ffbf91c448e24238c03c1b97
maxschoolgit <maxw76@nycstudents.net>
2025-12-05 13:03:40 -0500

    Update stopLight.py

--- stopLight.py
+++ stopLight.py
@@ -2,19 +2,19 @@ from tkinter import *
 #need to install on all machines
 from tkmacosx import Button
 
-
 # Create the main window
 root = Tk()
-root.title("Enter Title Here")
+root.title("Red Light, Green Light")
 
 #Set size of window
 root.geometry("500x400")
 
-
-# Create buttons
+# Create buttons/text
+text = Text(root, bg = 'grey', fg = 'black', bd = 5, font = 'Times')
 red_button = Button(root, text="Red", background='red')
 green_button = Button(root, text='Green', background='green')
 yellow_button = Button(root, text='Yellow', background='yellow')
+above_label = Label(root, text='This is a stoplight')
 
 #Add a label
 label = Label(root, text="Red Light, Green Light!")
@@ -24,6 +24,8 @@ label.pack()
 red_button.pack()
 green_button.pack()
 yellow_button.pack()
+above_label.pack()
+text.pack()
 
 
 # Start the GUI event loop
